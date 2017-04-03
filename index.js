2404f3a663c5f435366f2fb609f714288663c1ad
borantaylan <borantaylan@hotmail.com>
2017-04-03 17:00:15 +0200

    Tagesteller become Tage steller, also welcome message updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var moment = require('moment');
 
 
 app.launch(function(request, response) {
-    response.say('Welcome to your test skill').reprompt('Way to go. You got it to run. Bad ass.').shouldEndSession(false);
+    response.say('Willkommen im Techcafe!').reprompt('Way to go. You got it to run. Bad ass.').shouldEndSession(false);
 });
 
 
@@ -56,7 +56,9 @@ app.intent('getMenu', {
                     output = "";
                     $(this).find('.category').each(function() {
                         if ($(this).find('.category-content > p').length) {
-                            output = output + $(this).find('.category-title').html() + ":";
+                            var title = $(this).find('.category-title').html();
+                            if(title == "Tagesteller"){title = "Tage steller";}
+                            output = output + title + ":";
                             $(this).find('.category-content > p').each(function() {
                                 output = output + " " + $(this).html().replace(/ *\([^)]*\) */g, "");
                             })
